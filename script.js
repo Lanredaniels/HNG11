68208341b424bdb2bcc0674620bcdd5c3cfcbdb1
lanredaniels <lanredaniels29@gmail.com>
2024-07-03 07:14:28 +0000

    second commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const Name = "Olanrewaju Magbagbeola";
+    const username = "Lanre_daniels";
+    const Email = "lanredaniels29@gmail.com";
+
+    document.getElementById("Name").textContent = "Name: " + Name;
+    document.getElementById("Username").textContent = "Username: " + username;
+    document.getElementById("Email").textContent = "Email: " + Email;
+});
+
